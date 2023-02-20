2d6281808f20aa75df3e2e13646ba090aeb4daec
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-20 17:46:26 +0100

    Create haedder for todo list

---
+++ src/component/headder.js
@@ -0,0 +1,12 @@
+import './headder.css';
+
+const headder = () => {
+    return (
+        <>
+        <h1>Todos</h1>
+        <p>Items will persist in the browser local storage</p>
+        </>
+    )
+}
+
+export default headder;
